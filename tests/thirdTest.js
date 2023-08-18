549f21a6ff38a1189de6fe3c55f896b3d45e1194
Marcello <marcello.strategy@gmail.com>
2023-08-18 00:15:08 -0500

    New Selenium project to push to GitHub

---
+++ tests/thirdTest.js
@@ -0,0 +1,48 @@
+
+const {Builder,By,Key} = require('selenium-webdriver'); // selenium web driver
+var should = require('chai').should(); // chai assertion package - should style ( if you want to use expect style just dont add .should )
+
+// describe block
+describe("add todo test",function(){  // Test Suite
+
+    /*
+    this.timeout(30000)
+    let driver
+    let vars
+    beforeEach(async function() {
+        driver = await new Builder().forBrowser('chrome').build()
+        vars = {}
+    })
+    afterEach(async function() {
+        await driver.quit();
+    }) */
+
+    // it block
+    it("successfully adds a todo to application", async function(){  // Test
+
+        // launch the browser 
+        let driver = await new Builder().forBrowser("chrome").build();
+
+        // navigate to our application
+        await driver.get("https://lambdatest.github.io/sample-todo-app/");
+
+        // add a todo
+        await driver.findElement(By.id('sampletodotext')).sendKeys('Learn Selenium',Key.RETURN);
+
+        /** assertion to check if the text "Learn Selenium" was added **/
+
+        // find the last item on the list and retrieve it // retrieves the last item added to the list 
+        let todoText = await driver.findElement(By.xpath('//li[last()]')).getText().then(function(value){
+            return value
+        })   
+
+        // assert using chai should  ( should uses the value that you want to validate )
+        todoText.should.equal('Learn Selenium');
+
+        // close the browser
+        await driver.quit();
+        
+    });
+});
+
+
